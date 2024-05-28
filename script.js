ff71bddab9131fdb916e2e82728fb49701b79167
MateusCamargoS-1 <mateuscamargodasilva2@gmail.com>
2024-05-28 19:27:21 -0300

    atualizando informações

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let swiperTop = new Swiper(".mySwiperTop", {
-      loop: true,
-      loopedSlides: 10,
-      autoplay: {
-        delay: 7000,
-        disableOnInteraction: false,
-      },
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      spaceBetween: 0,
-      effect: "coverflow",
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+  let swiperTop = new Swiper(".mySwiperTop", {
+    loop: true,
+    loopedSlides: 10,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+    },
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
   });
+});
