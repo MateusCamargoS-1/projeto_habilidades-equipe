0b0a4869a96e0470e7af2c5a126b733bc17aeb8f
MateusCamargoS-1 <mateuscamargodasilva2@gmail.com>
2024-05-28 22:24:32 -0300

    voltando o efeito

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
     centeredSlides: true,
     slidesPerView: 'auto',
     spaceBetween: 0,
-    effect: "cards",
-    cardsEffect: {
-      perSlideOffset: 8,
-      perSlideRotate: 2,
-      rotate: true,
-      // modifier: 1,
-      slideShadows: true,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
     },
     pagination: {
       el: ".swiper-pagination",
